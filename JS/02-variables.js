ec274c2a105866b842ea574acb0fbce6b753370a
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-04 16:38:50 -0500

    se agrego variable con let y un alert para que arrojara el hola

--- JS/02-variables.js
+++ JS/02-variables.js
@@ -8,6 +8,8 @@ var continente = "America";
 var antiguedad = 2019;
 var pais_y_continente = pais+" "+continente;
 pais = "argentina";
+let prueba = "hola";
+alert("hola")
 continente = "america_del_sur"
 console.log(pais,continente,antiguedad);
 alert(pais_y_continente);
