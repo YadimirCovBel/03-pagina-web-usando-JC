e9580efac3aa6e7379ee080cad18a9ce1ddcaaab
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-04 16:52:21 -0500

    se agrego adecuadamente el js 03 let y var

---
+++ JS/03-let-y-var.js
@@ -0,0 +1,10 @@
+'use strict'
+//Puebas let y var
+
+var numero = 40;
+console.log(numero); //valor 40
+if(true)
+    var numero = 50;
+    console.log(numero); //valor 50
+    )
+    console.log(numero); //valor 50
