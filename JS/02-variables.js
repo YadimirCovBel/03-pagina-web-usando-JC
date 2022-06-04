6dc38b660b9638af77bfa23144d18a024a35711a
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-04 16:15:12 -0500

    se cambio el valor de las variables pais y continente y movio el string concatenado

--- JS/02-variables.js
+++ JS/02-variables.js
@@ -5,5 +5,7 @@ var pais = "Mexico";
 var continente = "America";
 var antiguedad = 2019;
 var pais_y_continente = pais+" "+continente;
+pais = "argentina";
+continente = "america_del_sur"
 console.log(pais,continente,antiguedad);
 alert(pais_y_continente);
