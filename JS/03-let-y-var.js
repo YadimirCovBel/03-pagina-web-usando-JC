921b461d46bb035469e33be9b6c1f44fd626987e
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-04 17:14:26 -0500

    se corrigio el if en la parte de pruenas de variables let

--- JS/03-let-y-var.js
+++ JS/03-let-y-var.js
@@ -1,10 +1,21 @@
 'use strict'
-//Puebas let y var
 
+//Puebas let y var
+//prueba con var
 var numero = 40;
 console.log(numero); //valor 40
-if(true)
+if(true){
     var numero = 50;
-    console.log(numero); //valor 50
-    )
-    console.log(numero); //valor 50
+    console.log(numero); //valor 50 
+}
+console.log(numero); //valor 50
+//Pruea con let
+var texto = "curso js Yadimir Covarrubias";
+console.log(texto); //valor js
+
+if(true){
+    let texto = "curso laravel 5 Yadimir Covarrbias"
+    console.log(texto); //valor laravel 5
+}
+
+console.log(texto);
