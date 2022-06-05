f4913127505581b503cb62901b0e228a66235bb1
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-05 17:27:11 -0500

    se borro el if de edad mayor que y se realizo un if en el que se concatenan var nombre string de texto y var edad

--- JS/06-condicionales.js
+++ JS/06-condicionales.js
@@ -1,10 +1,9 @@
 "use strict"
 
 //condicional if
-var edad1 = 30;
-var edad2 = 12;
-if(edad1>edad2){
-    console.log("edad1 es mayor que edad2");
-}else{
-    console.log("la edad1 es inferior a la edad2");
+var edad = 18
+var nombre = "Yadimir Covarrubias"
+
+if (edad >= 18){
+console.log(nombre+"tiene "+edad+" de edad")
 }
