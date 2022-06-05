08f11d4b53a2f30829eb705ee7b138b14afbf397
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-05 17:42:58 -0500

    se agrego un if dentro del principal if condiciona dentro de condicional

--- JS/06-condicionales.js
+++ JS/06-condicionales.js
@@ -1,12 +1,14 @@
 "use strict"
 
 //condicional if
-var edad = 18
+var edad = 33
 var nombre = "Yadimir Covarrubias"
 
 if (edad >= 18){
     //es mayor de edad
 console.log(nombre+"tiene "+edad+", es mayor de edad")
+if(edad<=33){ console.log("eres milenial")}
+
 }else{
     //es menor de edad
 console.log(nombre+"tiene "+edad+", es menor de edad")    
