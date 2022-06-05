c6d63b3c66a8bc9fcea21bd66741970318e09bcc
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-05 18:03:06 -0500

    se uso if dentro de otra condicional y tambien else if

--- JS/06-condicionales.js
+++ JS/06-condicionales.js
@@ -1,13 +1,17 @@
 "use strict"
 
 //condicional if
-var edad = 33
+var edad = 34
 var nombre = "Yadimir Covarrubias"
 
 if (edad >= 18){
     //es mayor de edad
 console.log(nombre+"tiene "+edad+", es mayor de edad")
-if(edad<=33){ console.log("eres milenial")}
+if(edad<=33){ console.log("eres milenial")
+}else if(edad>=70){ console.log("eres anciano")
+}else{
+console.log("ya no eres milenial")    
+}
 
 }else{
     //es menor de edad
