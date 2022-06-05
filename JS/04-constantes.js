1a913aa18247f25826ae9f8e3203f5f952627230
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-05 13:05:25 -0500

    se creo el archibo 04/constantes js para practicar constantes, se agregaron la var web y la const ip para despues modificar web para aplicar un console log

---
+++ JS/04-constantes.js
@@ -0,0 +1,9 @@
+"use strict"
+//constantes
+//es como una variable pero nunca cambia  
+var web = "https://www.google.com/"
+const ip = "66.249.93.104"
+
+web =  "https://news.google.com/"
+
+console.log(web,ip);  
