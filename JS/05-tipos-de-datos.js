f0c48d78385cac6e94168ce1819d89679a0f3bf7
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-06-05 15:38:44 -0500

    se escribieron tipos de datos

--- JS/05-tipos-de-datos.js
+++ JS/05-tipos-de-datos.js
@@ -1,4 +1,14 @@
 "use strict"
-alert ("hola");
 //operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+alert ("es el resultado de la operacion"+operacion)
+
 //tipos de datos
+var numero_entero = 44;
+var cadena_texto = "hola mundo"
+var verdadero_o_falso = true;
+var numero_falso = "33";
+
+console.log(numero_falso)
