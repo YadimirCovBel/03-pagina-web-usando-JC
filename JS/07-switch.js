c9c8b9c3516d93b49dbed7f5bee64bde09b93858
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-22 21:17:33 -0500

    se termino el ejercicio de switch ejemplo edades

--- JS/07-switch.js
+++ JS/07-switch.js
@@ -1,4 +1,23 @@
 'use strict'
 
 //switch
-console.log('switch')
+var edad = 10;
+var imprime = "";
+switch(edad) {
+	case 18:
+		imprime = "acabas de cumplir la mayoria de edad";
+	break;
+	case 25:
+		imprime = "ya eres un adulto";
+	break;
+	case 40:
+		imprime = "crisis de los cuarenta"
+	break;
+	case 75:
+		imprime = "ya eres un anciano"
+	break;
+	default:
+		imprime = "tu edad es neutra"
+	break;
+}
+console.log(imprime);
