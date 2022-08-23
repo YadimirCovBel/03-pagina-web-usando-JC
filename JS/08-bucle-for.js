ef3a5ea6457a13e976fb47c06f8caf3c49711063
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-22 21:35:46 -0500

    se agrego el file bucle tambien modificamos el script de index

---
+++ JS/08-bucle-for.js
@@ -0,0 +1,10 @@
+'use strict'
+// Bucle FOR
+/* es una estructura de 
+control que se repite varias veces
+*/
+var numeros = 100;
+
+for(var i = 0; i > numeros; i++){
+    console.log("camos por el numero"+i)
+}
