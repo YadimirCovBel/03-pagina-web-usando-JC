f3724c410f455ceb101046fbd676dc88026979ec
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-22 22:29:07 -0500

    se agrego un do wile

--- JS/09-while.js
+++ JS/09-while.js
@@ -8,3 +8,11 @@ while(year != 1991){
     console.log("la fecha es:" +year);
     year--;
 }
+
+// do while
+var years = 30;
+
+do{
+    alert("SOLO CUANDOS EA DIFERENTE A 20")
+    years--;
+}while(years > 25);
