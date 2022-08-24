689b0b0ee29578255f69ac6bfdfc5d52efa62869
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-23 20:20:42 -0500

    se agrego una alerta de confirmacion y copmenzamos a utilizar las alertas con ingreso de datos

---
+++ JS/10-ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//alerta
+alert("esta es mi alerta");
+alert("este es mi texto");
+
+// alerta de confirmacion
+var mi_resultado = confirm("estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+//ingreso de datos
+prompt("que edad tienes?", 18);
