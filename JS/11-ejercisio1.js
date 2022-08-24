6aa135bfe125c4bc32a96462d2a673352ae9c63d
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-23 21:09:36 -0500

    se termino el ejercisio 1 en el cual se introdusen dos numeros y evalua cual es mayor o iguales

---
+++ JS/11-ejercisio1.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/*
+programa que pida dos numeros y que nos diga cual es mayor 
+y cual es el menor o si son iguales
+*/
+
+var numero_1 = parseInt(prompt("introduce el primer numero", 0))
+var numero_2 = parseInt(prompt("introduce el segundo numero", 0))
+
+console.log(numero_1, numero_2)
+
+if(numero_1 == numero_2){
+    alert("los numeros son iguales");
+}else if(numero_1 > numero_2) {
+    alert("el numero mayor es:" + numero_1)
+    alert("el numero menor es:" + numero_2)
+}else if(numero_1 < numero_2){
+    alert("el numero mayor es:" + numero_2)
+    alert("el numero menor es:" + numero_1)
+}else{
+    alert("introduce numeros correctos");
+}
