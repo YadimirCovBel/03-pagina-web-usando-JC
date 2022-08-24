e99695f6da8fe35310be1141d73afff7b2d6f49c
Yadimir Covarrubias <Yadimir.cova@gmail.com>
2022-08-23 21:44:49 -0500

    se agrego un while para generar un bucle el cual exige numeros superiores a 0 y tambien vuelve a exigir numeros si agregan un NaN

--- JS/11-ejercisio1.js
+++ JS/11-ejercisio1.js
@@ -9,9 +9,11 @@ PLUS si los numeros no son un numero o son menores o iguales a 0 vuelve a pedir 
 var numero_1 = parseInt(prompt("introduce el primer numero", 0))
 var numero_2 = parseInt(prompt("introduce el segundo numero", 0))
 
-console.log(numero_1, numero_2)
-
-if(numero_1 == numero_2){
+while(numero_1 <= 0 || numero_2 < 0 || isNaN(numero_1) || isNaN(numero_2)){
+    var numero_1 = parseInt(prompt("introduce el primer numero", 0))
+    var numero_2 = parseInt(prompt("introduce el segundo numero", 0))
+}
+ if(numero_1 == numero_2){
     alert("los numeros son iguales");
 }else if(numero_1 > numero_2) {
     alert("el numero mayor es:" + numero_1)
