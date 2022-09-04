6d3282a1ac9217871332d7eea4547e7beacd6d1f
Yadimir <yadimir.cova@gmail.com>
2022-09-04 16:52:52 -0500

    se realizo el ejercisio 7 tablas multiplicadoras usando for se instrullo como poner mas de una cariable en un for usando varios for en el mismo codigo

--- js/17-ejercisio7.js
+++ js/17-ejercisio7.js
@@ -1,1 +1,21 @@
 'use strict'
+// tabla de multiplica de un numero introducido en pantalla
+
+
+
+var numero_1 = parseInt(prompt("de que numero quieres la tabla", 1))
+
+
+document.write("<h1> la tabla multiplicativa de  "+numero_1+" es:</h1>")
+    for (var i = 1; i <= 10; i++){
+        document.write(i+" x " +numero_1+" = "+(i*numero_1)+"<br/>");
+    
+    }   
+
+//todas las tablas de multiplicar
+for(var c = 1; c <= 10; c++){
+document.write("<h1> la tabla multiplicativa de  "+c+" es:</h1>");
+    for (var i = 1; i <= 10; i++){
+        document.write(i+" x " +c+" = "+(i*c)+"<br/>");
+    }
+}
