251423fd86bb146544d021d575383fda6ca30764
Yadimir <yadimir.cova@gmail.com>
2022-09-04 14:31:21 -0500

    se creo el file 15-ejercisio5.js y se modifico el index para funcionar con el mismo

--- js/14-ejercisio4.js
+++ js/14-ejercisio4.js
@@ -8,11 +8,11 @@ consiste en mostrar todos los numeros inpares introducidos que hay entre dos num
 var numero_1 = parseInt (prompt("introduce el primer numero", 0))
 var numero_2 = parseInt (prompt("introduce el primer numero", 0))
 
-document.write("<h1>De "+numero_1+" a "+numero_2+" estan estos numeros:</h1>")
+
 while (numero_1 < numero_2){
     numero_1++;
 
-    if(numero1%2 ! = 0){
+    if(numero_1 % 2 != 0){
         console.log("el"+numero_1+ "es impar")
     }
 
