c7fcac71017acf8e5e7a7bceec76b71ba0d2d4ff
Yadimir <yadimir.cova@gmail.com>
2022-09-04 14:56:07 -0500

    se termino el ejercisio 5 para mostrar los numeros divisores

--- js/15-ejercisio5.js
+++ js/15-ejercisio5.js
@@ -1,1 +1,10 @@
 'use strict'
+
+/* mostrara todos los numeros divisores de un numero introducido por pantalla
+*/
+var numero = parseInt(prompt("introdice un numero"));
+
+for(var i = 1; i <= numero; i++){
+    if(numero % i == 0)
+    console.log("divisores: " +i)
+}
