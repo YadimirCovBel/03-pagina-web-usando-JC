4dac8f8f9516a9cfe52c6b45bc2374fa7168d49c
Yadimir <yadimir.cova@gmail.com>
2022-09-04 15:43:04 -0500

    se realizo el ejercisio como se instrulle en el curso

---
+++ js/16-ejercisio6.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/* 
+hacer un programa que indique si es par o impar un numero,
+1 ventana que pida el numero
+2  si un numero no es valido que lo pida nuevamente
+3 que muestre si es par o impart
+*/
+
+
+var numero_1 = parseInt(prompt("introduce el primer numero", 0))
+
+
+while(isNaN(numero_1)){
+    var numero_1 = parseInt(prompt("introduce el primer numero", 0))
+}
+ if(numero_1 % 2 != 0){
+    alert("el numero " +numero_1+ " es impar");
+}else{
+    alert("el numero " +numero_1+ " es par")
+}
