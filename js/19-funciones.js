5a8104677dcadc3fc2855c67a3f3830cc966391b
Yadimir <Yadimir.cova@gmail.com>
2022-09-09 17:06:29 -0500

    se realizo el ejercisio de parametros opcionales y de funciones dentro de funciones

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -93,7 +93,7 @@ nuevo ejemplo codigo
 
 se puede ingresar la funcion dentro de un bucle en este caso un for
 */
-
+/*
 function calculadora(numero_1, numero_2){
 
     console.log("suma: "+ (numero_1+numero_2));
@@ -108,7 +108,72 @@ for(var i = 1; i <= 10; i++){
 console.log(i)
     calculadora(i, 8)
 }
+
 /*
  haremos que mientras que la i sea menor a 10 se repetira la funcion, introducimos la funcion dentro del
 bucle donde 1 sera el numero_1 y 8 el numero_2
 */
+
+/* 
+pareamentros opcionales
+son parametros que no es obligatorio meterle resultado si no que ya vienen algo por defecto
+en este caso mostrar = false
+*/
+/*
+function calculadora(numero_1, numero_2, mostrar = false){
+
+    if(mostrar == false){
+    console.log("suma: "+ (numero_1+numero_2));
+    console.log("resta: "+ (numero_1-numero_2));
+    console.log("multiplicasion: "+ (numero_1*numero_2));
+    console.log("division: "+ (numero_1/numero_2));
+        console.log("*solo soy una separacion ********************************************************")
+    }else{
+        document.write("suma: "+ (numero_1+numero_2) +"<br/>");
+        document.write("resta: "+ (numero_1-numero_2) +"<br/>");
+        document.write("multiplicasion: "+ (numero_1*numero_2) +"<br/>");
+        document.write("division: "+ (numero_1/numero_2) +"<br/>");
+        document.write("*solo soy una separacion ********************************************************")
+            
+    }
+    }
+
+    calculadora(10, 8)
+    calculadora(5, 2, true)
+    //se agrego un true para que se aplique el else mostrandoen la pagina
+*/
+
+/* 
+funciones dentro de funciones
+*/
+
+function PorConsola(numero_1, numero_2){
+    console.log("suma: "+ (numero_1+numero_2));
+    console.log("resta: "+ (numero_1-numero_2));
+    console.log("multiplicasion: "+ (numero_1*numero_2));
+    console.log("division: "+ (numero_1/numero_2));
+        console.log("*solo soy una separacion ********************************************************")
+   
+}
+function PorPantalla(numero_1, numero_2){
+
+        document.write("suma: "+ (numero_1+numero_2) +"<br/>");
+        document.write("resta: "+ (numero_1-numero_2) +"<br/>");
+        document.write("multiplicasion: "+ (numero_1*numero_2) +"<br/>");
+        document.write("division: "+ (numero_1/numero_2) +"<br/>");
+        document.write("*solo soy una separacion ********************************************************")
+            
+    }
+    function calculadora(numero_1, numero_2, mostrar = false){
+        if(mostrar == false){
+            PorConsola(numero_1, numero_2)
+
+    }else{
+            PorPantalla(numero_1, numero_2)
+    }
+        return true;
+    }
+    calculadora(10, 8)
+    calculadora(5, 2, true)
+    calculadora(20,1)
+    calculadora(3, 2, true)
