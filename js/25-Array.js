feb32d4a15b4e90968de9ebef413568849d99c5a
Yadimir <yadimir.cova@gmail.com>
2022-09-19 19:09:12 -0500

    se esta realizando el episodio de Arrays avanzados, en el mismo se realizo un promp para sacar un elemento espesifico del array, como plus agrege que al ingresar un numero superior vuelva a paedir el numero despues de dar el aviso que no debe superar un numero espesifico

--- js/25-Array.js
+++ js/25-Array.js
@@ -5,7 +5,6 @@
 puedo tener muchos valores dentro de una variable y esos valores estan almacenados  almacenados dentro de una matris dentro de un conjunto
 de valores.
 
-*/
 var nombre = "Yadimir Covarrubias"
 //  un Array se puede definir con [] los corchetes, un Array es una coleccion de varios datos teniendo una variable con valores
 var nombres = ["Yadimir Covarrubias", "Yumaira Rios", "Carlos Quintero", "Paul Covarrubias", 52, true];
@@ -15,7 +14,7 @@ se pueden meter todo tipo de datos dentro de una Array texto, numericos, buleano
 
 sepueden definir el Array con los corchetes o definirlos en forma de objetos
 cada uno de estos valores estan guardados dentro de un indice  en la programacion los indices inician en 0 por lo tanto PHP seria el 0, JS 1, Go 2, Java 3
-*/
+
 var lenguajes = new Array("PHP", "JS", "Go", "Java");
 
 console.log(nombres)
@@ -25,3 +24,44 @@ console.log(lenguajes)
 // para poder acceder a alguna variable del Array en espesifico se requiere introducir su numero dentro del indice en este caso seleccionaremos el 2
 console.log(nombres[2]);
 // para seleccionar una variable espesifica debe de introducirse entre [] corchetes el numero de variable que se utilizaria.
+
+/* 
+array avanzados
+
+*/
+
+var nombre = "Yadimir Covarrubias"
+var nombres = ["Yadimir Covarrubias", "Yumaira Rios", "Carlos Quintero", "Paul Covarrubias", 52, true];
+var lenguajes = new Array("PHP", "JS", "Go", "Java");
+
+//console.log(nombres.length);
+// ccuando usamos .length aplicado a un Array lo que hace es mostrar la cantidad de elementos en el mismo en vez de la cantidad de caracteres. 
+
+var elemento = parseInt(prompt("Que elemento del Array quieres?", 0));
+//para poder sacar un elemento espesifico del indice del array se usa un prompt con el cual se escoge el numero que se ocupa segun el numero del indice  
+
+/*
+alert(nombres[elemento]);
+// colocamos un alert que trabaje con la variable elemento y esta es a la que se le introduciera el numero y arojara una opccion del indice
+*/
+    
+if(elemento >= nombres.length){
+/* 
+colocando nombres.length lo que hacemos es que el programa vea cuantas variables hay en el array en este caso 6  y toma ese numero para que elemento 
+no sea mayor o igual a ese
+*/
+alert("Introdusca un numero correcto entre las variables debe ser menor a: " + nombres.length); 
+// colocamos un alert que muestre un popup si es un numero fuera de las opcciones
+
+
+var elemento = parseInt(prompt("Que elemento del Array quieres?", 0));
+// plus agrege  para que pida nuevamente el numero
+
+alert("El usuario seleccionado es: " + nombres[elemento]);
+// plus para que arroje el resultado ingresado en la ventana despues del aviso de que debe ser un numero menor a nombres.length 
+
+}else{
+    alert("El usuario seleccionado es: " + nombres[elemento]);
+
+}
+//para impedir que resolver el que un usuario introdusca un numero fuera de las elecciones en el indice podemos usar un if 
