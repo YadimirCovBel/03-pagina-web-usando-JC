3ab6e9280685a0f135c1d7f5feaf1e576a9f1ef2
Yadimir <yadimir.cova@gmail.com>
2022-10-14 13:12:55 -0500

    se termino la sesion de dome ahora estamos en espesificasiones de query selector dentro deldome, tambien se realizaron modificasiones en el index dando clases a algunos elementos para seleccionarlos

--- js/28-DOM.js
+++ js/28-DOM.js
@@ -9,12 +9,14 @@ y manipularlos. de forma que si tenemos en la pagina algo se pueden manipular su
 function CambiaColor(color){
     caja.style.background = color;
 }
+// conseguir elementos con id espesifico
+
 
 // var caja = document.getElementById("MiCaja"); esta es la forma inicial que utilizamos para seleccionar un elemento
 var caja  = document.querySelector("#MiCaja")
 // con document.querySelector("#MiCaja")  podemos seleccionar por medio del id un elemento de la pagina web esta es la forma recomendada 
 // con la # seleccionamos por medio de id si quisieramos seleccionar por medio del elemento tendriamos que colocar un . en su lugar
-caja.innerHTML = "TEXO EN LA CAJA DESDE JS"
+caja.innerHTML = "TEXO EN LA CAJA DESDE JS";
 caja.style.background = "green";
 caja.style.padding = "20px";
 caja.style.color = "white";
@@ -76,3 +78,107 @@ lo cual permitira que se realizen cambios de color desde la terminal de forma li
 
 con caja.className = "hola"; podemos agregar al las clases de lo seleccionado
 */
+
+
+// como podria econtrar todos los dips en mi HTML
+
+// conseguir elementos por su etiqueta
+var TodosLosDivs = document.getElementsByTagName('div');
+// con document.getElementsByTagName('div) busca la consola y genera una array de los divs
+
+//si quiero acceder al segundo elemento seugn el indice tenemos que escribir  TodosLosDivs[2] con lso corchetes escogemos el elemento a trabajar
+var ContenidoEnTexto = TodosLosDivs[2].textContent;
+//usando .textContent podemos sacar del elemento el texto que contiene y mostrarlo con un console.log
+TodosLosDivs[2].innerHTML = "otro texto para el segundo elemento";
+// con InnerHTML modificamos el texto en la web en este caso cambiamos el soy el primer div por otro texto para el segundo elemento
+TodosLosDivs[2].style.background = "green";
+// mientras seleccionemos la variable TodosLosDivs y en Corchetes [] coloquemos el numero dos todas las modificasiones se realizaran solo a ese elemento
+
+console.log(TodosLosDivs);
+console.log(ContenidoEnTexto)
+//TodosLosDivs.array.forEach((valor, indice) => {
+    // forEach() ejecuta la función callback una vez por cada elemento del array, este solo sirve para recorrer arrays que creamos con valores, strings, etc
+    var seccion = document.querySelector("#MiSeccion") 
+    // !!!!!!!!!!!!!tambien agrego   var seccion = document.querySelector("#MiSeccion") fuera de camara!!!!!!!!!!!!!!!1
+   var hr = document.createElement("hr");
+    var valor;
+    for(valor in TodosLosDivs){
+        //utilizaremos un forin ya que en este caso es un html
+ console.log(TodosLosDivs[valor])
+ if(typeof TodosLosDivs[valor].textContent == 'string'){
+        var parrafo = document.createElement("p");
+    //.createElement("p"); es para crear un eleemento en este caso P que seria parrafo
+    var texto = document.createTextNode(TodosLosDivs[valor].textContent);
+    /*.createTextNode(valor);  agregar un texto a la pagina web y seleccionando (TodosLosDivs[valor].textContent); se selecciona el array de todos los divs
+    ingresamos la Variable Valor la cual tomara el valor que le llege de cada uno de los dips y con textcontent sacamos el texto dentro del array 
+    despues lo ponemos en la web
+    */
+
+    parrafo.append(texto);
+    /*
+    El método appendChild() inserta un nuevo nodo dentro de la estructura DOM de un documento, y es la segunda parte del proceso central uno-dos, 
+    crear-y-añadir para construir páginas web a base de programación.
+    
+    usaremos append en lugar de apendChild ya que nos e ocupa el apend child append significa agregar antes
+    */
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11document.querySelector("#MiSeccion").prepend(parrafo); por alguna razon quita esto en el curso y lo sustituye fuera de camara
+// prepend es agegar antes 
+    seccion.append(parrafo)
+    // !!!!!!!!!!!!!!!!!!!!!! se agrego      seccion.append(parrafo) fuera de camara!!!!!!!!!!!!!!!!
+ 
+}
+    }
+    seccion.append(hr);
+    //hr es para poner una linea divisoria
+//ctodos los documentos del doc son un array, 
+//con un forEach  creare una lista agregando el create element 
+// puedo meto el texto dentro del parrafo con parrafo.appendChild(texto)
+
+
+/*
+se agrego una seccion en el indice 
+
+		<section id ="MiSeccion">
+			<hr>
+		</section>
+!!!!!!!!!!!!!!!!!tambien por alguna razon fuera de camara borro el <hr> !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+y lo sustituimos por 
+
+
+		<section id ="MiSeccion">
+		
+			<h1>Listado de textos de los divs</h1>
+		
+		</section>
+        
+*/
+
+
+
+
+
+
+
+
+
+// conseguir elementos por su classe
+
+/* se le agrego clases en el index 
+		<div class="amarillo">Hola Soy Una Caja!</div>
+		<div class="rojo"> Soy el primer div!</div>
+		<div class="rojo"> yo soy el segundo div!</div>
+para poder buscarla
+        */
+var divsRojos = document.getElementsByClassName('rojo')
+//creamos una variabl y le ingresamos document.getElementsByClassName('rojo') se deberan seleccionar todos los elementos con la clase rojo
+var div;
+//definimos la variable div para usarlo en el bucle
+for(div in divsRojos){
+// vamos a crear un bucle para que cambie el background de los divsRojos
+if(divsRojos[div].className == "rojo"){
+// agregamos un if para que solo recorra los elementos rojo .className == "rojo") definimos que revise si su class name es rojo para que lo prosese
+    divsRojos[div].style.background = "red";
+// con esto se cambia el color del background  a rojo
+}
+}
+console.log(divsRojos)
