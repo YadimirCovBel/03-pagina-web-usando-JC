58e39040a5c27641c57d9333a450f1d67c70cc00
Yadimir <yadimir.cova@gmail.com>
2022-11-09 12:26:33 -0600

    se creo el archivo 36-fetch.js para trabajar con la pagina datos-externos

---
+++ js/36-fetch.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+alert("hola")
+/*
+Fetch(sustituto de ajax) y peticiones a servicios / apis rest
+
+es un api res es un servicio o un backend nos devuelve datos en json  puede 
+recibir peticiones de metodos http
+
+recibira una informacion y devolvera otra, mediante a ese api o backend se 
+estaran guardando datos . 
+
+*/
