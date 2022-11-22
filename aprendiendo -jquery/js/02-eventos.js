96f9ba4bdc7f900a259623427fbb2de02f049d7b
Yadimir <yadimir.cova@gmail.com>
2022-11-22 13:24:31 -0600

    se ha terminado la sesion 99 blur y focus donde aprendimos a modificar el borde de un input y tambien usar la etiqueta .val para extraer los datos de un imput y mostrarlos en un div

--- aprendiendo -jquery/js/02-eventos.js
+++ aprendiendo -jquery/js/02-eventos.js
@@ -64,8 +64,72 @@ Clase 97 HOVER
         
                 sesion 98 click y doble click
 
+          //click 
+
+        caja.click(function(){
+            $(this).css("background", "blue")
+                    .css("color", "white")
+                    ;
+        })
+
+        con caja.click(function()) agregamos el evento a realizar despues de dar click en el 
+    elemento seleccionado ene ste caso es la caja y cambiara de color el fonod y la letra
+
+        // doble click
+
+        caja.dblclick(function(){
+            $(this).css("background", "pink")
+                    .css("color", "yellow")
+                    ;
+        })
+        con  caja.dblclick(function(){ agregamos el evento a realizar despues de dar doble
+    click sobre el elemento seleccionado en este caso cambiara el color del fondo y letra
 
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            
+            sesion 99 eventos focus y blur
+
+    agregamos un form y un input al index para trabajar esta sesion dentro del html
+    
+            input{
+            padding: 10px;
+            width: 300px;
+            font-size: 20px;
+        }
+
+        <form>
+        <input type="text" id="nombre"/>
+    </form>
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            html
+ margin-top: 10px; 
+
+        es para agregar margen en este caso a un div creado con #datos
+
+ display: none;
+
+ es para ocultar algo en la pagina web y podemos hacer que se muestre despues de 
+ ejecutar algo.
+
+ $("#datos").show(); hace que se muestre el div escondido 
+
+   nombre.blur(function(){
+            $(this).css("border", "1px solid #ccc");
+            $("#datos").show();
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+ahora lo que haremos es que despues de salir del imput se muestre el div escondido
+exibiendo los datos que se inserten en el imput 
+
+nombre.blur(function(){
+            $(this).css("border", "1px solid #ccc");
+            $("#datos").text($(this).val()).show();
+        })
+usando .text($(this).val()).show()
+
+logramos que el texto insertado se meustre en el div usando text($(this).val())
 
 */
 $(document).ready(function(){
@@ -100,7 +164,7 @@ $(document).ready(function(){
         //hover
     caja.hover(cambiaRojo, cambiaVerde);
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-
+    //eventos click y doble ckick
         //click 
 
         caja.click(function(){
@@ -115,7 +179,16 @@ $(document).ready(function(){
                     .css("color", "yellow")
                     ;
         })
-
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //eventos focus y blurr
+        var nombre = $("#nombre");
+        nombre.focus(function(){
+            $(this).css("border", "5px solid green");
+        })
+        nombre.blur(function(){
+            $(this).css("border", "1px solid #ccc");
+            $("#datos").text($(this).val()).show();
+        })
 
     alert("eventos cargado")
 });
