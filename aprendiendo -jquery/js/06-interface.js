074489e3e68dd052c5d7be5e4bcd2cc6d9297363
Yadimir <yadimir.cova@gmail.com>
2022-11-29 17:51:03 -0600

    se termino la sesion 113 selectable donde creamos una lista le dimos styles para selecting y selected los cuales cambian el color del elemento

--- aprendiendo -jquery/js/06-interface.js
+++ aprendiendo -jquery/js/06-interface.js
@@ -34,8 +34,45 @@ cuadro y tambien con float: left los ordenamos uno al lado del otro izquierda
         <link rel="stylesheet" href="js/jquery-ui-1.13.2/jquery-ui.theme.min.css"/>
 
 
+                //      redimencionar elementos seleccionados
+
+        elemento.resizable();
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ 113 selectable
+
+    agregamos una lista en el html y le dimso style a esto con la class 
+    lista-seleccionable
+
+    .lista-seleccionable li{
+        width: 80%;
+        padding: 20px;
+        border: 1px solid #ccc;
+        list-style: none;
+        cursor: pointer;
+        
+        
+            list-style: none; 
+        con este le quitamos la numeracion o bullets
+
+        TAMBIEN LE PUSIMSO 
+                cursor: pointer;
+            este lo que hace es que cuando el mouse esta sobre el elemento cambia
+            a la mano 
+    
+    TAMBINE LE DAREMOS STYLE A ul.ui
+
+          ul.ui-selecting {background-color: green;}
+        ul.ui-selected {background-color: blue;}
+        
+
+
+
+
 */
-var elemento = $('.elemento')
+var elemento = $('.elemento');
+var lista = $(".lista-seleccionable");
 
 //.draggable permite que los elementos seleccionados puedan ser movidos por 
 // por el usuario en toda la web
@@ -45,6 +82,10 @@ elemento.draggable();
 //      redimencionar elementos seleccionados
 
 elemento.resizable();
+
+//seleccionar elementos
+lista.selectable();
+
 
 
     alert("todo listo");
