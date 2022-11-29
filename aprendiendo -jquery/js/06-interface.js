93ca9602f782b2edf4ab5c5ed3416af75cc4a4a1
Yadimir <yadimir.cova@gmail.com>
2022-11-29 17:29:48 -0600

    agregamos en el html los links stylesheet para poder realizar la accion de .resizable la cual permite modificar las dimenciones de los elementos seleccionados, terminamos la sesion 112. resizable

--- aprendiendo -jquery/js/06-interface.js
+++ aprendiendo -jquery/js/06-interface.js
@@ -17,12 +17,34 @@ seison 111
 creamos 3 divs con la class elemento con estos style les damos una forma de 
 cuadro y tambien con float: left los ordenamos uno al lado del otro izquierda
 
+        var elemento = $('.elemento')
+
+        //.draggable permite que los elementos seleccionados puedan ser movidos por 
+        // por el usuario en toda la web
+                                //mover elementos por la pagina 
+        elemento.draggable();
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+112 resizable
+            para poder modificar las dimenciones de los elementos seleccionaddos
+            primero tenemos que agregar los stylesheet de jQuery ui en el html
+
+        <link rel="stylesheet" href="js/jquery-ui-1.13.2/jquery-ui.min.css"/>
+        <link rel="stylesheet" href="js/jquery-ui-1.13.2/jquery-ui.structure.min.css"/>
+        <link rel="stylesheet" href="js/jquery-ui-1.13.2/jquery-ui.theme.min.css"/>
+
+
 */
 var elemento = $('.elemento')
 
 //.draggable permite que los elementos seleccionados puedan ser movidos por 
 // por el usuario en toda la web
+                        //mover elementos por la pagina 
 elemento.draggable();
+
+//      redimencionar elementos seleccionados
+
+elemento.resizable();
 
 
     alert("todo listo");
