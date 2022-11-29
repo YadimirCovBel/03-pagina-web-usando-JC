2cd49f94bb79d990e90c3d20cdc34da64b0adba4
Yadimir <yadimir.cova@gmail.com>
2022-11-29 17:19:51 -0600

    se termino la sesion 111.draggable donde aprendimos a usar .draggable que permite al usuario mover lo seleccionado con esta etiqueta por toda la web

--- aprendiendo -jquery/js/06-interface.js
+++ aprendiendo -jquery/js/06-interface.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+seison 111
+
+
+            <style>
+                .elemento{
+                    width: 300px;
+                    height: 200px;
+                    border: 4px solid black;
+                    background-color: lightblue;
+                    margin:  20px;
+                    float: left;
+                }
+            </style>
+creamos 3 divs con la class elemento con estos style les damos una forma de 
+cuadro y tambien con float: left los ordenamos uno al lado del otro izquierda
+
+*/
+var elemento = $('.elemento')
+
+//.draggable permite que los elementos seleccionados puedan ser movidos por 
+// por el usuario en toda la web
+elemento.draggable();
+
 
     alert("todo listo");
 })
