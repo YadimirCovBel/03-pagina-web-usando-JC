d45435c0ba2b83db6846fff966482081c5881627
Yadimir <yadimir.cova@gmail.com>
2022-12-02 16:24:20 -0600

    iniciamos el proyecto de creacion para una pagina web como ejercisio, en este caso abrimos una nueva carpeta llamada proyecto-js dentode la cual creamos la carpeta css, imagenes, js dentro la carpeta js creamos el main.js y dentro de css styles.css

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/*
+en el index pondremos un contenedor global 
+
+        <section id="global">
+                    
+                </section>
+seria la caja blanca que contiene toda la web
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+<header></header>
+
+para el emcabezado
+
+
+
+
+*/
