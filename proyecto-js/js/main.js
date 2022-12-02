741ede2c100975a8a1360b6408ae1158f21c20ba
Yadimir <yadimir.cova@gmail.com>
2022-12-02 17:08:54 -0600

    hicimos apuntes en main.js, trabajamos prinsipalmente en styles.css donde definimos el style del body, #global, #logo, * y .clearfix

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -13,7 +13,34 @@ seria la caja blanca que contiene toda la web
 
 para el emcabezado
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+CSS 
+    le da unsombreado 
+        box-shadow: 2px 2px 2px gray; 
+
+    #logo{
+     font-family: "Verdana", "Arial";
+     // da la tipografia
+     font-size: 30px;
+     //da el tama;o de la letra
+     letter-spacing:  4px;
+     //espacio entre letas
+     background: #235e3d;
+     //fondo
+     color: white;
+     //color de la letra
+     text-align: center;
+     //alineado al centro
+     width: 385px;
+     height: 80px; 
+}
+
+para aplicar style a todo lo de la pagina web se usa el *{}
+
+
+
+agregamso todos los styles de la pagina  
 
 
 */
