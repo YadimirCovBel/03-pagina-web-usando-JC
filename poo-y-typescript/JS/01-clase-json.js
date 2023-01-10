9ba1cd4b9afc03b6334288628654d0342875c1ae
Yadimir <yadimir.cova@gmail.com>
2023-01-10 17:11:58 -0600

    se agrego una fincion en el jason bicicleta

--- poo-y-typescript/JS/01-clase-json.js
+++ poo-y-typescript/JS/01-clase-json.js
@@ -2,7 +2,16 @@ var bicicleta = {
     color:'rojo',
     modelo: 'BMX',
     frenos: 'de disco',
-    velocidadMAxima: '60km/h'
+    velocidadMAxima: '60km/h',
+
+    //agregamos una funcion de callback para quese aplique 
+    //un cambio de color 
+    cambiaColor: function(nuevo_color){
+        //funciona escribiendo diretamente la variable, caracteristica y accion
+//    bicicleta.color = nuevo_color;
+        //tambien se realiza usando this
+        this.color = nuevo_color;
+}
 
 };
 
