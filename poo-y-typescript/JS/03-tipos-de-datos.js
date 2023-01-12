1e80c7816a90e91a080602d047e1209c2e8cd72d
Yadimir <yadimir.cova@gmail.com>
2023-01-12 12:13:53 -0600

    se aplico el tsc a la carpeta donde tenemos 03-tipos-de-datos.ts lo cual genero el js con el cual funcionara el index

---
+++ poo-y-typescript/JS/03-tipos-de-datos.js
@@ -0,0 +1,4 @@
+//string
+// de esta forma se define la variable y tipo de dato en el TS
+var cadena = "solo puedo agregar un string aqui";
+console.log(cadena);
---
+++ poo-y-typescript/JS/03-tipos-de-datos.ts
@@ -0,0 +1,7 @@
+//string
+// de esta forma se define la variable y tipo de dato en el TS
+let cadena: string = "solo puedo agregar un string aqui"
+
+
+
+console.log(cadena);
