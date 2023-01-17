c3b4a9de7125ce698e91a7d7b16d605d38588bbc
Yadimir <yadimir.cova@gmail.com>
2023-01-16 21:32:04 -0600

    terminamos la sesion del cuerso 156 clases

--- poo-y-typescript/JS/camisetas.js
+++ poo-y-typescript/JS/camisetas.js
@@ -3,8 +3,49 @@
 //la primera letra 
 var Camiseta = /** @class */ (function () {
     function Camiseta() {
+        //propiedades (caracteristicas del objeto)
+        // en este caso definimos que es una propiedad publica se llamara color
+        // y sera tipo string
+        this.color = "";
+        this.talla = "";
+        this.tipo = "";
+        // al agregar = "" le definimos que al ejecutar tendra un valor
+        this.precio = 0;
     }
+    //las propiedades pueden ser public, private o protected
+    //public se peude acceder desde cualquier sitio
+    //protected se puede acceder desde la classe que lo define
+    //private  solo son accesible desde la clase que los define
+    //por lo que no se pueden dar valores desde fuera entonces 
+    // se requiere un metodo acontinuacion.
+    //metodos (que son funciones o acciones que hara el objeto)
+    Camiseta.prototype.setColor = function (color) {
+        this.color = color;
+    };
+    Camiseta.prototype.getColor = function () {
+        return this.color;
+    };
     return Camiseta;
 }());
-new camiseta = new Camiseta();
-console.log(Camiseta);
+// ya teniendo el model (classe) podemos crear objetos con facilidad
+var camiseta = new Camiseta();
+camiseta.setColor("Rojo");
+/*al volver las propiedades de la funcion privada este metodo
+deja de ser funcional y utilizaremos otro
+camiseta.color = "Rojo";
+camiseta.modelo = "Manga Larga";
+camiseta.marca = "Nike";
+camiseta.talla = "L";
+camiseta.precio = 10;
+*/
+var playera = new Camiseta();
+playera.setColor("Azul");
+/*al volver las propiedades de la funcion privada este metodo
+deja de ser funcional y utilizaremos otro
+playera.color = "Azul";
+playera.modelo = "Manga corta";
+playera.marca = "Adidas";
+playera.talla = "L";
+playera.precio = 15;
+*/
+console.log(camiseta.getColor(), playera);
--- poo-y-typescript/JS/camisetas.ts
+++ poo-y-typescript/JS/camisetas.ts
@@ -7,19 +7,59 @@ class Camiseta{
     //propiedades (caracteristicas del objeto)
     // en este caso definimos que es una propiedad publica se llamara color
     // y sera tipo string
-    public color: string;
-    public modelo: string;
-    public marca: string;
-    public talla: string;
-    public precio: number;
+    private color:string ="";
+    private talla:string = "";
+    private tipo:string = "";
+    // al agregar = "" le definimos que al ejecutar tendra un valor
+    private precio:number = 0;
+
+    //las propiedades pueden ser public, private o protected
+    //public se peude acceder desde cualquier sitio
+
+    //protected se puede acceder desde la classe que lo define
+    
+    //private  solo son accesible desde la clase que los define
+    //por lo que no se pueden dar valores desde fuera entonces 
+    // se requiere un metodo acontinuacion.
 
     //metodos (que son funciones o acciones que hara el objeto)
 
+    public setColor(color){
+        this.color = color;
+
+    }
+    public getColor(){
+        return this.color
+    }
 
 }
 
+// ya teniendo el model (classe) podemos crear objetos con facilidad
 
+var camiseta = new Camiseta();
 
-new camiseta = new Camiseta();
+camiseta.setColor("Rojo")
 
-console.log(Camiseta);
+/*al volver las propiedades de la funcion privada este metodo
+deja de ser funcional y utilizaremos otro
+camiseta.color = "Rojo";
+camiseta.modelo = "Manga Larga";
+camiseta.marca = "Nike";
+camiseta.talla = "L";
+camiseta.precio = 10;
+*/
+
+var playera = new Camiseta();
+
+playera.setColor("Azul")
+
+/*al volver las propiedades de la funcion privada este metodo
+deja de ser funcional y utilizaremos otro
+playera.color = "Azul";
+playera.modelo = "Manga corta";
+playera.marca = "Adidas";
+playera.talla = "L";
+playera.precio = 15;
+*/
+
+console.log(camiseta.getColor(),playera);
