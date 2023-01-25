4e7edb29f23389001128bc18b9792602cd420ece
Yadimir <yadimir.cova@gmail.com>
2023-01-25 17:48:40 -0600

    agregamos angular para realizar el aprendizaje del mismo y creamos un file en la carpeta app de angular llamada videojuego y dos files llamados videojuego.component.html y videojuego.component.ts

---
+++ aprendiendo-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'Bien Benido a el curso de JS y estamos aprendiendo-angular';
+}
