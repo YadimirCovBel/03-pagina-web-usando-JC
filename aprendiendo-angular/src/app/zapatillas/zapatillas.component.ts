98dfac430973685668be4ff176823d12c425eaff
Yadimir <yadimir.cova@gmail.com>
2023-01-26 16:57:51 -0600

    se termino la sesion 172. multiples/varios compoenentes donde creamos el compoenten zapatillas  y lo utilizamos

---
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component ({
+
+    selector: 'zapatillas',
+    templateUrl: './zapatillas.component.html'
+
+})
+
+export class ZapatillasComponent{
+    public titulo: string;
+
+    constructor(){
+
+        this.titulo = "componente de zapatillas";
+    }
+
+}
