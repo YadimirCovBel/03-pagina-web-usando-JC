2d37fc040083cad1cd40e4ab781f019b3629b0a2
Yadimir <yadimir.cova@gmail.com>
2023-02-15 11:49:50 -0600

    se realizo un two way data binding en la sesion 181 y 182

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
@@ -12,10 +13,11 @@ import { CursosComponent } from './cursos/cursos.component';
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent,
+    CursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -10,18 +10,25 @@ import { zapatilla } from "../models/zapatilla";
 export class ZapatillasComponent implements OnInit {
     public titulo: string;
     public zapatillas: Array<zapatilla>;
+    public marcas: string[];
+    public color: string;
+    public mi_marca: string;
 
     constructor(){
+
+        this.color = 'blue';
+        this.marcas = new Array();
         this.titulo = "componente de zapatillas";
         //  al tener definido el molde zapatilla  y agregarlo 
         //como array  tenemos que ingresar lo en el constructor 
         this.zapatillas = [
-            new zapatilla('AirMax', 'nike', 'roja', 190, true ),
+            new zapatilla('AirMax', 'nike', 'roja', 40, true ),
             new zapatilla('rebook clasic', 'rebook', 'blanco', 80, true ),
             new zapatilla('runner', 'nike', 'negro', 60, true ),
+            new zapatilla('jordan', 'nike', 'negro', 200, false ),
             new zapatilla('yizze', 'adidas', 'gris', 180, true )
         ];
-
+        this.mi_marca = "fila";
 
 
         console.log("se ha cargado el compoente : zapatillas.component.ts");
@@ -29,5 +36,26 @@ export class ZapatillasComponent implements OnInit {
 
     ngOnInit() {
         console.log(this.zapatillas);
+        this.getMarcas();
     }
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            console.log(index);
+            }
+        });
+        console.log(this.marcas);
+    }
+
+    getMarca(){
+        alert(this.mi_marca);
+        console.log(this.mi_marca);
+    }
+
+    addMArca(){
+        this.marcas.push(this.mi_marca);
+    }
+
+
 }
