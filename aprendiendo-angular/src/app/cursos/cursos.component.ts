fd42dada16dbbb92a339eb313073ab396bff3fe2
Yadimir <yadimir.cova@gmail.com>
2023-02-21 16:20:38 -0600

    terminamos sesion 191 redirecciones router navigate creamos un boton para redireccionar en la web y tambien desde el url

--- aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -17,7 +17,13 @@ export class CursosComponent {
       this.nombre = params['nombre'];
       this.followers = +params['followers'];
         console.log(typeof+params['followers']);
+        if(this.nombre == 'ninguno'){
+          this._router.navigate(['/home']);
+        }
     });
   }
 
+  redirigir(){
+    this._router.navigate(['/zapatillas']);
+  }
 }
