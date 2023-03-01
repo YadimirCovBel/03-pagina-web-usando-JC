8ca449fd1a29279238a4f94deaf3068c3794d1a2
Yadimir <yadimir.cova@gmail.com>
2023-03-01 13:15:01 -0600

    se termino la sesion 195 servicios y HttpClient donde usamos un repositorio para extraer usuarios y mostrarlos en la pagina

--- aprendiendo-angular/src/app/externo/externo.component.ts
+++ aprendiendo-angular/src/app/externo/externo.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionesService } from '../services/peticiones.services';
 
 @Component({
   selector: 'externo',
   templateUrl: './externo.component.html',
-  styleUrls: ['./externo.component.css']
+  styleUrls: ['./externo.component.css'],
+  providers: [PeticionesService]
 })
-export class ExternoComponent {
+export class ExternoComponent implements OnInit{
+
+  public user: any;
+  public userId: any;
+
+  constructor(
+    private _peticionesService: PeticionesService
+  ){
+    this.userId = 1;
+  }
+
+  ngOnInit(){
+    this.cargaUsuario();
+  }
+
+  cargaUsuario (){
+    this._peticionesService.getUser(this.userId).subscribe(
+      result => {
+        this.user = result.data;
+        console.log(result);
+        
+      },
+      error => {
+       console.log(<any>error); 
+      }
+    );
+
+  };
 
 }
