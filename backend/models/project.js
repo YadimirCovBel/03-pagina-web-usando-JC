44fe3c00e56acbe96e87d8c3990058971543ab28
Yadimir <yadimir.cova@gmail.com>
2023-04-11 17:53:26 -0600

    se trabajo en project.js agregamos var mongoos, Schema, ProjectSchema definiendo que datos se deben agregar y el tipo de datos son tambien exportamos

--- backend/models/project.js
+++ backend/models/project.js
@@ -1,1 +1,16 @@
 'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+//definimos esquema 
+
+var ProjectSchema = Schema({
+    name:String,
+    description:String,
+    categoty:String,
+    lang:[String],
+    year:Number
+});
+
+module.exports = mongoose.model('projects', ProjectSchema);
+// projects --> guarda los documentos en la coleccion
