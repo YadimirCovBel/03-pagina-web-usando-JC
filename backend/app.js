a61bf09eec5c7ab9a01fea941e6af6bdb4db2d11
Yadimir <yadimir.cova@gmail.com>
2023-04-10 18:51:09 -0600

    se definio app.use con body parser buscando que convierta todo lo recibido a json

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+//bodyParser es para convertir lo que llega en un json
 var app = express();
 
-//rutas
+//Cargar Archivos de rutas
 
 //middlewares
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+//convertira todo lo que llegue a un json
+
+
+//CORS
+
+//RUTAS
