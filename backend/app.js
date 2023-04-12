337eb408d11f25cc7f9143a86b83c802ee7f4137
Yadimir <yadimir.cova@gmail.com>
2023-04-11 18:07:26 -0600

    se  eliminaron de app.js las rutas anteriores

--- backend/app.js
+++ backend/app.js
@@ -17,26 +17,7 @@ app.use(bodyParser.json());
 //CORS
 
 //RUTAS
-app.get('/', (req, res) => {
-    //req request datos que puedo enviar desde el cliente
-    //res responce la respuesta que se enviara 
-    res.status(200).send(
-       "<h1>Pagina de Inicio</h1>"
-    );
-});
-app.post('/test/:id', (req, res) => {
-    //req request datos que puedo enviar desde el cliente
-    //res responce la respuesta que se enviara 
-    console.log(req.body.nombre);
-    //body es un parametro que se envia por post en el body
-    console.log(req.query.web);
-    //query un parametro enviado por get en la url
-    console.log(req.params.id);
-    // params un parametro como forma de la ruta
-    res.status(200).send({
-        message: "hola Mundo desde mi API de NodeJS"
-    });
-});
+
 
 
 
