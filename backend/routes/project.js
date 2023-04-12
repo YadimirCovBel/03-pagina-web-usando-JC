774f3dc43eb31ae4f5fc64797b896bfe5698488f
Yadimir <yadimir.cova@gmail.com>
2023-04-11 18:26:46 -0600

    definimos route get de home donde solicitaremos que utilice el objeto ProjectControler y acceda al metodo home

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -3,3 +3,7 @@
 var express = require('express');
 var ProjectController = require('../controllers/project');
 
+var router = express.Router();
+
+router.get('/home', ProjectController.home)
+//requerimos que utilice el objeto ProjectController y acceda al metodo home
