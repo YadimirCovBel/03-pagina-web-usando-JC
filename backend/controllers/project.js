6b25e9b9c7df6d66457e5c6dbaeeaba67be533c1
Yadimir <yadimir.cova@gmail.com>
2023-04-12 18:38:08 -0600

    se definieron en el file project dentro del folder controllers la variable project y params

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -18,9 +18,21 @@ var controller = {
     saveProject: function(req, res){
         var project = new Project();
 
+        var params = req.body;
+        project.name = params.name;
+        project.description= params.description;
+        project.categoty= params.categoty;
+        project.lang= params.lang;
+        project.year= params.year;
+        project.image = null;
+
+       
+
+
         return res.status(200).send({
+            params: params,
             message: "Metodo saveProject"
-            
+
         })
     }
 
