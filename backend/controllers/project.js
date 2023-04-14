ac7d62864a18a2ea00774f1d32035bf46e5847de
Yadimir <yadimir.cova@gmail.com>
2023-04-13 20:03:42 -0600

    se genero el getProject definiendo la funcion para buscar por Id

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,4 +1,5 @@
 'use strict'
+const project = require('../models/project');
 var Project = require('../models/project')
 var controller = {
 
@@ -42,7 +43,7 @@ var controller = {
         });
             */
 
-        //nuevo metodo se tubo que usar un try y un catch para que funcionara 
+        //nuevo metodo para guardar documentos se tubo que usar un try y un catch para que funcionara 
         try{
             await project.save();
             return res.status(200).send({
@@ -52,6 +53,19 @@ var controller = {
                 message: 'Error al guardar el objeto', error:err});
         }
     },
+
+        getProject: function(req, res){
+                var projectId = req.params.id;
+
+                Project.findById(projectId, (err, project)=>{
+                    if(err) return res.status(500).send({message: 'error al devolver los datos'});
+                    if(!project) return res.save(404).send({message: 'el proyecto no existe.'});
+                    return res.status(200).send({
+                        project
+                    });
+ 
+                });
+        }
 };
 
 module.exports = controller;
