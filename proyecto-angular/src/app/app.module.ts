3e5656364dab9203cfa9f8036daaf879a1fbdbaa
Yadimir <yadimir.cova@gmail.com>
2023-05-01 11:55:06 -0600

    se genero y corrigio el nombre de proyecto-angualr

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    ComponentsComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto-angular/src/app/app.routing.ts
@@ -0,0 +1,2 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
