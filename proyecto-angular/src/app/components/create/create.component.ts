d9f09d60e01a54eb5edc4152f97452f55a143848
Yadimir <yadimir.cova@gmail.com>
2023-05-16 18:45:05 -0600

    se hacen pruebas con el back end y el create component ya que no arroja la informacion requerida

--- proyecto-angular/src/app/components/create/create.component.ts
+++ proyecto-angular/src/app/components/create/create.component.ts
@@ -10,23 +10,24 @@ import { ProjectService } from 'src/app/services/project.service';
 })
 export class CreateComponent implements OnInit {
 
-  public title:string;
-  public project:Project;
+  public title: string;
+  public project: Project;
+  public status: string;
 
   constructor(
     private _projectService: ProjectService
   ){
     this.title="Creat proyecto"
     this.project= new Project('','','','',2023,'','');
-
+    this.status= "";
     /*
-     public _id: string,
-        public name: string,
-        public description: string,
-        public categoty: string,
-        public year:Number,
-        public lang:String,
-        public image: String*/ 
+      public _id: string,
+      public name: string,
+      public description: string,
+      public categoty: string,
+      public year:Number,
+      public lang:String,
+      public image: String*/ 
     
   }
   ngOnInit(): void {
@@ -34,16 +35,21 @@ export class CreateComponent implements OnInit {
   }
   onSubmit(form:any){
     console.log(this.project)
-    this._projectService.saveProject(this.project).subscribe(
-        response => {
-            console.log(response);
-        }, 
-        error => {
+    this._projectService.saveProject(this.project).subscribe({
+        next:(response) => {
+            if(response.project){
+              this.status = "success";
+            }else{
+              this.status= "failed";
+            }
+            console.log(this.status);
+            console.log(response.project._id);
+        },
+        error:(error) => {
           console.log(<any>error);
         }
-      );
-    
-      
+        
+    });
   }
 
 }
